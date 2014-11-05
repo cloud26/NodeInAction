d8d16dfb5a5569c8e34c0a2ab5add6181e41e01f
cloud26 <c_loud26@163.com>
2014-11-05 14:26:39 +0800

    add user model

--- 09_express/shoutbox/lib/user.js
+++ 09_express/shoutbox/lib/user.js
@@ -2,47 +2,72 @@
 * @Author: cloud26
 * @Date:   2014-11-04 00:09:00
 * @Last Modified by:   cloud26
-* @Last Modified time: 2014-11-04 00:42:05
+* @Last Modified time: 2014-11-05 14:25:50
 */
-/*
+
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
 
 mongoose.connect('mongodb://localhost/shoutbox_app');
-module.exports = User;
 
-function User(obj){
-	for(var key in obj) {
-		this[key] = obj[key];
-	}
-}
+var UserSchema = new mongoose.Schema({
+	name: String,
+	pass: String,
+	salt: String
+});
 
-User.prototype.save = function(next){
-	if (this.id){
-		this.update(next);
-	} else {
-		var user = this;
-		user.hashPassword(function(err){
-			user.save();
-		});
-	}
-}
+//module.exports = mongoose.model('User', User);
 
-User.prototype.update = function(next){
-	var user = this;
-
-}
-
+mongoose.model('User', UserSchema);
+var User = mongoose.model('User');
 User.prototype.hashPassword = function(next){
 	var user = this;
 	bcrypt.genSalt(12, function(err, salt){
 		if(err)	return next(err);
 		user.salt = salt;
-		bcrypt.hash(user.password, salt, function(err, hash){
+		bcrypt.hash(user.pass, salt, function(err, hash){
 			if(err)	return next(err);
-			user.password = hash;
+			user.pass = hash;
+			next();
+		}); 
+	});
+};
+
+var tobi = new User();
+tobi.name = 'tom';
+tobi.pass = 'tom';
+/*
+tobi.hashPassword(function(err){
+	if(err)	throw err;
+	tobi.save(function(err){
+		if(err) throw err;
+		console.log('User saved');
+		console.log(tobi.name);
+		console.log(tobi.pass);
+		console.log(tobi.salt);
+	});
+});
+*/
+
+function authenticate(name, pass, next){
+	User.findOne({'name': name}, function(err, user){
+		if(err)	next(err);
+		if(!user.id)	return next();
+		bcrypt.hash(pass, user.salt, function(err, hash){
+			if(err)	return next(err);
+			if(hash == user.pass)
+				return next(null, user);
 			next();
 		});
 	});
-}
-*/
+};
+//
+authenticate('tom', 'xx', function(err, user){
+	if(err)	throw err;
+	if(!user)	
+		console.log('User not exists!');
+	else {
+		console.log(user.name);
+		console.log(user.salt);
+	}
+});
